c8881f7190a12dfbac7efd54823ad34c33a27031
Nikhil Raj <nikhilrajpandey1@gmail.com>
2019-09-22 12:18:47 +0530

    Added Enemy in game

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -9,15 +10,25 @@ game_over = False
 screen_width = 800
 screen_height = 800
 game_title = "Blaster"
+
 player_position_x = 400
 player_position_y = 600
-player_height = 40
+height = 40  # One Box Height and width
 player_velocity = 0  # Player will start from rest
 fps = 30
+enemy_position_x = random.randint(0,750)
+enemy_position_y = 20
+enemy_velocity = 20
+
+enemy_list = []
 
 pygame.display.set_caption(game_title)
 screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
+
+def plot_enemy():
+    for enemy_x,enemy_y in enemy_list:
+        pygame.draw.rect(screen,black,[enemy_x,enemy_y,height,height])
 
 while not game_over:
     for event in pygame.event.get():
@@ -25,13 +36,30 @@ while not game_over:
             game_over = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                player_velocity = 15
+                player_velocity = 20
             if event.key == pygame.K_LEFT:
-                player_velocity = -15
-    screen.fill(white)  # Most important step
-    player = pygame.draw.rect(screen,black,[player_position_x,player_position_y,player_height,player_height])
+                player_velocity = -20
 
-    # Updating the position of the player
+    screen.fill(white)  # Most important step
+
+    # Declaring Enemy and player
+    player = pygame.draw.rect(screen,black,[player_position_x,player_position_y,height,height])
+    enemy_list.append([enemy_position_x,enemy_position_y])
+
+    # If it was not written then enemy will become so long
+    if len(enemy_list) > 2:
+        del enemy_list[0]
+
+    # Updating the position of the player and enemy
+    plot_enemy()
     player_position_x = player_position_x + player_velocity
+    enemy_position_y = enemy_position_y + enemy_velocity
+
+    # Taking Enemy Position 0 when it collides
+    if enemy_position_y >= screen_height:
+        enemy_position_y = 0
+        enemy_position_x = random.randint(0,750)
+
+
     pygame.display.update()
     clock.tick(fps)
