8c193d6479794be4a053e9004c8bb175497f02ec
Nikhil Raj <nikhilrajpandey1@gmail.com>
2019-09-22 22:50:14 +0530

    Missile Bug solved in which when the missile hits enemy then enemy become large and all the logic made

--- main.py
+++ main.py
@@ -18,17 +18,17 @@ player_position_x = 400
 player_position_y = 600
 height = 40  # One Box Height and width
 player_velocity = 0  # Player will start from rest
-fps = 60
+fps = 120
 
 enemy_position_x_list = [random.randint(50,750),random.randint(50,750),random.randint(50,750)]
 enemy_height = 60
-enemy_position_y = 20
-enemy_velocity = 10
+enemy_position_y_list = [20,20,20]
+enemy_velocity = 5
 enemy_list = []
 
 missile_height = 10
 missile_position_list = []
-missile_velocity = 12
+missile_velocity = 6
 pygame.display.set_caption(game_title)
 screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
@@ -50,18 +50,18 @@ while not game_over:
             game_over = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                player_velocity = 10
+                player_velocity = 5
             if event.key == pygame.K_LEFT:
-                player_velocity = -10
+                player_velocity = -5
             if event.key == pygame.K_RCTRL:
                 missile_position_list.append([player_position_x,player_position_y,True])
                 fire()
 
     # Declaring Enemy and player
     player = pygame.draw.rect(screen,black,[player_position_x,player_position_y,height,height])
-    enemy_list.append([enemy_position_x_list[0],enemy_position_y])
-    enemy_list.append([enemy_position_x_list[1],enemy_position_y])
-    enemy_list.append([enemy_position_x_list[2], enemy_position_y])
+    enemy_list.append([enemy_position_x_list[0],enemy_position_y_list[0]])
+    enemy_list.append([enemy_position_x_list[1],enemy_position_y_list[1]])
+    enemy_list.append([enemy_position_x_list[2], enemy_position_y_list[2]])
 
     if len(enemy_list) >= 6:  # If it was not written then enemy will become so long
         del enemy_list[0]
@@ -73,7 +73,10 @@ while not game_over:
 
     # Updating the position of the player,enemy and Missile
     player_position_x = player_position_x + player_velocity
-    enemy_position_y = enemy_position_y + enemy_velocity
+
+    for enemy_position_y in enemy_position_y_list:
+        current_position_index = enemy_position_y_list.index(enemy_position_y)
+        enemy_position_y_list[current_position_index] = enemy_position_y_list[current_position_index] + enemy_velocity
 
     count_missile = 0  # In this for loop the enumerate function is not working so i used this
 
@@ -95,22 +98,28 @@ while not game_over:
             game_over = True
 
     no_of_kills = 0
+
     # Destroying Enemy when missile hit them
     for missile_count,(missile_position_x, missile_position_y, launched) in enumerate(missile_position_list):
+        deleted_missile = False
         if launched:
             for enemy_count,(enemy_x, enemy_y) in enumerate(enemy_list):
-                if abs(enemy_x - missile_position_x) < 30 and abs(enemy_y - missile_position_y) < 30:  # Means Missile hit the enemy
-                    print("hello world")
+                if abs(enemy_x - missile_position_x) < 30 and abs(enemy_y - missile_position_y) < 30 and not deleted_missile:  # Means Missile hit the enemy
                     curent_enemy_index = enemy_list.index([enemy_x, enemy_y])
                     del enemy_list[curent_enemy_index]
                     del missile_position_list[missile_position_list.index([missile_position_x,missile_position_y,launched])]
+                    deleted_missile = True
                     enemy_position_x_list[curent_enemy_index] = random.randint(50,750)
-                    enemy_position_y = 0
+                    enemy_position_y_list[curent_enemy_index] = 20
 
     # Taking Enemy Position 0 when it collides
-    if enemy_position_y >= screen_height:
-        enemy_position_y = 0
-        enemy_position_x_list = [random.randint(50,750),random.randint(50,750),random.randint(50,750)]
+    for enemy_position_y in enemy_position_y_list:
+        if int(enemy_position_y) >= screen_height:
+            index_of_current_position = enemy_position_y_list.index(enemy_position_y)
+            enemy_position_y_list[index_of_current_position] = 20
+            enemy_position_x_list[index_of_current_position] = random.randint(5,750)
+
+    enemy_list = []
 
     pygame.display.update()
     clock.tick(fps)
